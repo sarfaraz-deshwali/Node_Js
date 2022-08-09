8b2d6c10e5d6d01fbd2645c2e8a3fd214ef749c4
sarfaraz-deshwali <102010896+sarfaraz-deshwali@users.noreply.github.com>
2022-08-09 18:40:42 +0530

    express add

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-
 const express = require('express')
+const path = require('path')
 const app = express()
 const port = 3000
 
-app.get('/',(req,res)=>{
+app.use(express.static(path.join(__dirname,'public')))
 
-  res.send('hello world')
-  console.log(req,url)
+app.get('/hello/:name', (req, res) => {
+  res.send('Hello World!' + req.params.name)
 })
-app.listen(port)
+
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname,'indexe.html'))
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
--- website.js
+++ website.js
@@ -1,19 +1,30 @@
+const fs = require('fs');
 const http = require('http');
-
 const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Content-Type', 'text/html');
  
   console.log(req.url)
   if(req.url=='/'){
     res.statusCode = 200;
     res.end('Hello World');
   }
+  else if(req.url=='/hello'){
+    res.statusCode = 200;
+    const data = fs.readFileSync('hello.html')
+    res.end(data.toString());
+  }
   else if(req.url=='/about'){
     res.statusCode = 200;
-    res.end('Hello World about');
+    const data = fs.readFileSync('about.html')
+    res.end(data.toString());
+  }
+  else if(req.url=='/main'){
+    res.statusCode = 200;
+    const data = fs.readFileSync('main.html')
+    res.end(data.toString());
   }
 });
 
