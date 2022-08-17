bac58db60e63732bc6645fa59e1847012d933c44
sarfaraz-deshwali <102010896+sarfaraz-deshwali@users.noreply.github.com>
2022-08-17 14:48:40 +0530

    rdr

--- event.js
+++ event.js
@@ -1,13 +1,46 @@
-const EventEmitter = require('events');
+/*const EventEmitter = require('events');
 
 class MyEmitter extends EventEmitter {}
 
-const myEmitter = new MyEmitter();
+const myEmitter = new MyEmitter();*/
 
-myEmitter.on('waterFull', () => {
+/* myEmitter.on('waterFull', () => {
   console.log('please turn off the moter');
   setTimeout(()=>{
     console.log('please save the water please')
   },3000)
-});
-myEmitter.emit('waterFull');
+});*/
+
+/* myEmitter.on('tank',()=>{
+console.log('there is a tankk men')
+setTimeout(() => {
+  console.log('belive me men ')
+}, 3000);
+})*/
+
+/* myEmitter.on('choti appi',()=>{
+  console.log('roti khalo ayan pintu')
+  setTimeout(() => {
+    console.log('aawa appi')
+  }, 2000);
+})*/
+/*
+myEmitter.on('jetalal',()=>{
+  console.log('chup hoja sathvi fail')
+setTimeout(()=>{
+  console.log('ana mana eru ahh ghs heua ')
+} ,3000)
+})
+myEmitter.emit('jetalal');*/
+
+const EventEmitter = require('events');
+class MyEmitter extends EventEmitter {}
+const myEmitter = new MyEmitter();
+
+  myEmitter.on('jetalal',()=>{
+    console.log('papu ji m dukan ja raha hu jai jenendra')
+    setTimeout(()=>{
+      console.log('thik hai jai jenendra')
+    },4000)
+  })
+  myEmitter.emit('jetalal');
--- fsmodules.js
+++ fsmodules.js
@@ -7,7 +7,6 @@ let text = fs.readFileSync("file.txt","utf-8")
 let new_text = text.replace('help' , 'kick')
 console.log(new_text)
 
-
 let txt = fs.readFileSync("file.txt","utf-8")
 let nw_txt = text.replace('okkK!','dum')
 console.log(nw_txt)
@@ -16,3 +15,7 @@ console.log(nw_txt)
 let tt = fs.readFileSync('file.txt','utf-8');
 let n_tt = text.replace('help','check');
 console.log(n_tt)
+ 
+let t = fs.readFileSync('file.txt',);
+let n_t = text.replace('me','you');
+console.log(n_t)
